9d12f115e464d62af03984c209909226d166c90c
Cameron Kilgore <2545084k@student.gla.ac.uk>
2022-02-08 10:08:34 +0000

    final commit

--- populate_rango.py
+++ populate_rango.py
@@ -14,34 +14,34 @@ def populate():
     # through each data structure, and add the data to our models.
 
     python_pages = [
-        {'title': 'Official Python Tutorial', 'url':'http://docs.python.org/3/tutorial/'},
-        {'title':'How to Think like a Computer Scientist', 'url':'http://www.greenteapress.com/thinkpython/'},
-        {'title':'Learn Python in 10 Minutes', 'url':'http://www.korokithakis.net/tutorials/python/'} ]
+        {'title': 'Official Python Tutorial', 'url':'http://docs.python.org/3/tutorial/', 'views': 100},
+        {'title':'How to Think like a Computer Scientist', 'url':'http://www.greenteapress.com/thinkpython/', 'views': 20},
+        {'title':'Learn Python in 10 Minutes', 'url':'http://www.korokithakis.net/tutorials/python/', 'views': 50} ]
 
     django_pages = [
-        {'title':'Official Django Tutorial', 'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-        {'title':'Django Rocks', 'url':'http://www.djangorocks.com/'},
-        {'title':'How to Tango with Django', 'url':'http://www.tangowithdjango.com/'} ]
+        {'title':'Official Django Tutorial', 'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/', 'views': 2},
+        {'title':'Django Rocks', 'url':'http://www.djangorocks.com/', 'views': 15},
+        {'title':'How to Tango with Django', 'url':'http://www.tangowithdjango.com/', 'views': 30} ]
     
     other_pages = [
-        {'title':'Bottle','url':'http://bottlepy.org/docs/dev/'},
-        {'title':'Flask','url':'http://flask.pocoo.org'} ]
+        {'title':'Bottle','url':'http://bottlepy.org/docs/dev/', 'views': 1},
+        {'title':'Flask','url':'http://flask.pocoo.org', 'views': 21} ]
         
-    cats = {'Python': {'pages': python_pages}, 'Django': {'pages': django_pages}, 'Other Frameworks': {'pages': other_pages} }
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes":64}, "Django": {"pages": django_pages, "views": 64, "likes":32}, "Other Frameworks": {"pages": other_pages, "views": 32, "likes":16} }
     
     # If you want to add more categories or pages,
     # add them to the dictionaries above.
     # The code below goes through the cats dictionary, then adds each category,
     # and then adds all the associated pages for that category.
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
-        for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+        c = add_cat(cat,cat_data)
+        for p in cat_data["pages"]:
+            add_page(c, p["title"], p["url"], p["views"])
             
     # Print out the categories we have added.
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
-            print(f'- {c}: {p}')
+            print("-{0})-{1}".format(str(c), str(p)))
             
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
@@ -50,8 +50,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
     
-def add_cat(name):
+def add_cat(name, cat_data):
     c = Category.objects.get_or_create(name=name)[0]
+    c.likes = cat_data["likes"]
+    c.views = cat_data["views"]
     c.save()
     return c
     
